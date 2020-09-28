7b2f597393fd92135ff79f3804bdff711f626dd3
deleonAndroid3 <alolzz123@gmail.com>
2020-09-28 18:07:18 +0800

    Added some test in e2e test script

--- test/singletest.js
+++ test/singletest.js
@@ -19,6 +19,36 @@ var driver = wd.promiseRemote('http://hub-cloud.browserstack.com/wd/hub')
 driver
   .init(desiredCaps)
   .then(function () {
+    return driver.elementByXPath(
+      '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.widget.EditText[1]',
+      asserters.isDisplayed && asserters.isEnabled,
+      30000,
+    )
+  })
+  .then(function (username) {
+    return username.sendKeys('deleon.johncarlo2016@gmail.com')
+  })
+  .then(function () {
+    return driver.elementByXPath(
+      '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.widget.EditText[2]',
+      asserters.isDisplayed && asserters.isEnabled,
+      30000,
+    )
+  })
+  .then(function (password) {
+    return password.sendKeys('Carlo123')
+  })
+  .then(function () {
+    return driver.elementByXPath(
+      '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup',
+      asserters.isDisplayed && asserters.isEnabled,
+      30000,
+    )
+  })
+  .then(function (login) {
+    return login.click()
+  })
+  .then(function () {
     return driver.waitForElementByAccessibilityId(
       'Type a message...',
       asserters.isDisplayed && asserters.isEnabled,
